8bea373dfb4650e5f326157f6e217e7b75d2ee3c
Umesh Dewasinghe <89865946+umeshdewasinghe@users.noreply.github.com>
2022-12-09 00:19:11 +0530

    first commit

---
+++ src/Point3D.java
@@ -0,0 +1,31 @@
+import java.math.*;
+public class Point3D {
+    private int coordinate1;
+    private int coordinate2;
+    private int coordinate3;
+
+    public Point3D() {
+        this.coordinate1 = 0;
+        this.coordinate2 = 0;
+        this.coordinate3 = 0;
+    }
+
+    public Point3D(int coordinate1, int coordinate2, int coordinate3) {
+        this.coordinate1 = coordinate1;
+        this.coordinate2 = coordinate2;
+        this.coordinate3 = coordinate3;
+    }
+
+    public double distance (int coordinate1, int coordinate2, int coordinate3){
+
+
+        return Math.sqrt((coordinate1 * coordinate1) + (coordinate2 * coordinate2) + (coordinate3 * coordinate3));
+
+    }
+
+    public void display(double distance){
+
+
+        System.out.println("Distance from the origin is : "+distance);
+    }
+}
